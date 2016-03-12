88fb50dc04307fee00e9b44ade5fd72ae167383d
kevin <mattisonii@yahoo.com>
2016-03-12 15:26:28 -0800

    Add concatenation functionality.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 //throw your require variables up here!
 
-gulp.task('jsBrowserify', function(){
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+  .pipe(concat('allConcat.js'))
+  .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'], function(){
   return browserify({ /*this is a key*/entries:
-    ['./js/browser.js'] /*this is an object*/})
+    ['./tmp/allConcat.js'] /*this is an object*/})
 
   //calls browserify function defined above. Tells it which files to browserify in brackets.
     .bundle() //function passed to the result of browserify
